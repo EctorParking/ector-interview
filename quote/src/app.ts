4a547bb37532384884ccdf4736be7b4e905db0e7
Jimmy Delas <jimmy.delas@gmail.com>
2022-03-21 18:38:15 +0100

    Improve quote test

--- quote/src/app.ts
+++ quote/src/app.ts
@@ -1,12 +1,15 @@
 import Koa from 'koa';
+import koaBody from 'koa-body';
 
 const app = new Koa();
 
-app.use(async (ctx) => {
-  ctx.body = 'Hello World';
+app
+  .use(koaBody())
+  .use(async (ctx) => {
+    ctx.body = 'Hello World';
+  });
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('🚀 Ready on port 3000');
 });
-
-app.listen(3000);
-
-// eslint-disable-next-line no-console
-console.log('🚀 Ready on port 3000');
--- quote/src/service/db.ts
+++ quote/src/service/db.ts
@@ -1,8 +1,15 @@
-const mysql = require('mysql2');
+import mysql from 'mysql2/promise';
 
-export default mysql.createConnection({
+const conn = mysql.createConnection({
   host: 'HOST',
   user: 'USER',
   password: 'PWD',
   database: 'DB',
 });
+
+/*
+* See https://www.npmjs.com/package/mysql2#using-promise-wrapper
+*/
+export const query = async (sql:string) => (await conn).query(sql);
+
+export default conn;
