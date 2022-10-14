168d541870a34e66993b0e0f53a7fce8f4788aa2
Jimmy Delas <jimmy.delas@gmail.com>
2022-10-14 09:43:03 +0200

    Enhance quote test

--- quote/src/app.ts
+++ quote/src/app.ts
@@ -1,13 +1,13 @@
 import Koa from 'koa';
 import koaBody from 'koa-body';
 
+import helloRouter from './controller/hello';
+
 const app = new Koa();
 
 app
   .use(koaBody())
-  .use(async (ctx) => {
-    ctx.body = 'Hello World';
-  });
+  .use(helloRouter.routes());
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
