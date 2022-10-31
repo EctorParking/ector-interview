0de19f989f7451124d391b1d237ca6536856a527
Jimmy Delas <jimmy.delas@gmail.com>
2022-10-31 15:45:33 +0100

    GPS exercice solution

--- gps/main.ts
+++ gps/main.ts
@@ -3,7 +3,25 @@ import map, { Cities, City, Connexion } from './map';
 const getDistance = (path: Connexion[]) => path.reduce((total, { distance }) => total + distance, 0);
 
 const path = (from: keyof Cities, to: keyof Cities): Array<Connexion> => {
-  return [];
+  const recursion = (conn: Connexion, path: Array<Connexion>, visited: Array<City>): Connexion[] | null => {
+    if (visited.indexOf(conn.from) === -1) {
+      path.push(conn);
+      visited.push(conn.from);
+      if (conn.to === map.cities[to]) {
+        return path;
+      }
+      return conn.to.neighbours.reduce<Connexion[] | null>((bestPath, conn: Connexion) => {
+        const testPath = recursion(conn, [...path], [...visited]);
+        return testPath && (!bestPath || getDistance(testPath) < getDistance(bestPath)) ? testPath : bestPath;
+      }, null);
+    }
+    return null;
+  };
+
+  return map.cities[from].neighbours.reduce<Connexion[] | null>((bestPath, conn: Connexion) => {
+    const testPath = recursion(conn, [], []);
+    return testPath && (!bestPath || getDistance(testPath) < getDistance(bestPath)) ? testPath : bestPath;
+  }, null) || [];
 }
 
 console.log('----- Test 1 -----');
