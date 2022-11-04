47c7adf6c64e4d0a1b36ac4ce949d36dba8b74b9
CamilleINGOUF <camille.ingouf@ectorparking.com>
2022-11-04 11:06:04 +0100

    solution camille

--- gps/main.ts
+++ gps/main.ts
@@ -1,8 +1,28 @@
 import map, { Cities, Connexion } from './map';
 
-const path = (from: keyof Cities, to: keyof Cities): Array<Connexion> => {
-  return [];
+const lengthPath = (path: Array<Connexion>): number => path.reduce((acc, cur) => acc + cur.distance, 0)
+
+const recursivePath = (from: keyof Cities, to: keyof Cities, path: Array<Connexion>): Array<Connexion> => {
+  const neighbours = map.cities[from].neighbours;
+  const routes = neighbours
+    .filter((n) => !path.find(p => p.from.name == n.to.name || p.to.name == n.to.name))
+    .map((n) => {
+      if (n.to.name == to) {
+        return [...path, n]
+      }
+      return recursivePath(n.to.name, to, [...path, n])
+    })
+    .filter((r) => lengthPath(r) > 0)
+  if (routes.length === 0) return [];
+  return routes.reduce((acc, cur) => lengthPath(acc) < lengthPath(cur) ? acc : cur, routes[0])
 }
+
+const path = (from: keyof Cities, to: keyof Cities): Array<Connexion> => {
+  return recursivePath(from, to, []);
+}
+
+console.log('----- Test 0 -----');
+map.showPath(path('Lille', 'Bruxelle'));
 
 console.log('----- Test 1 -----');
 map.showPath(path('Lille', 'Lyon'));
